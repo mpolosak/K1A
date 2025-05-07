d0ec5b7c3a1b789e79f025a9060951e4dd93dc08
Michał Połosak <mpolosak@op.pl>
2025-05-07 17:48:18 +0200

    Add AppointmentsController and endpoints

---
+++ K1A/Controllers/AppointmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace K1A.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class AppointmentsController : ControllerBase
+{
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetAppointmentAsync(int id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAppointmentAsync()
+    {
+        return Created("/api/appointments/id", null);
+    }
+}
